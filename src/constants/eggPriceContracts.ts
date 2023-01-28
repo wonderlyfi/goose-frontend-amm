3f6e56aa2646b77fad1af99b0bf283db5e6d30d6
wonderlyfi <wonderlyfi@proton.me>
2023-01-27 20:41:16 -0500

    fml

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {frostAddress: string, usdcAddress: string, lpAddress:string} = {
   frostAddress: '0x0c7f27Ce61202BC614D4A52D26240de014FC56D2',
   usdcAddress: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0xA2705689878C00Bc0a3D818CA697667344D4c509'
 }
 
 export default priceContracts
